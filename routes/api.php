a108813bccf5394184a02a9491c2b6502f8d2c61
YooDaehan <daehan821@g.yju.ac.kr>
2024-02-25 16:28:39 +0900

    ✨ feat : 회원탈퇴 구현

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SocialController;
 use App\Http\Controllers\AuthController;
-
+use App\Http\Controllers\ImageTranslationController;
 use App\Http\Controllers\VocabularyNoteController;
 
 /*
@@ -28,7 +28,9 @@ Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::middleware('auth:sanctum')->group(function () {
   Route::post('/logout', [AuthController::class, 'logout']);
   Route::post('/refresh', [AuthController::class, 'refreshToken']);
+  Route::post('/signOut', [AuthController::class, 'signOut']);
 });
+Route::post('/image/translate',[ImageTranslationController::class, 'translateImage']);
 
 Route::group(
   ['middleware' => ['web']],
