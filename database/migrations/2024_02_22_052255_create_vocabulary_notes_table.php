5c42cb2808ad417c8a8b59e3c0b6a8ee51a686f4
YooDaehan <daehan821@g.yju.ac.kr>
2024-04-04 20:10:14 +0900

    🔨 Refactor : 각종 오류 수정

--- database/migrations/2024_02_22_052255_create_vocabulary_notes_table.php
+++ database/migrations/2024_02_22_052255_create_vocabulary_notes_table.php
@@ -6,30 +6,30 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('vocabulary_notes', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade')->onUpdate('cascade');
-            $table->foreignId('level_id')->constrained('levels')->onDelete('cascade')->onUpdate('cascade')->default('7');
-            $table->string('title');
-            $table->json('gana');
-            $table->json('kanji');
-            $table->json('meaning');
-            $table->boolean('is_public')->default(false);
-            $table->boolean('is_creator')->default(false);
-            $table->timestamps();
-        });
-    }
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void
+	{
+		Schema::create('vocabulary_notes', function (Blueprint $table) {
+			$table->id();
+			$table->foreignId('user_id')->constrained('users')->onDelete('cascade')->onUpdate('cascade');
+			$table->foreignId('level_id')->constrained('levels')->onDelete('cascade')->onUpdate('cascade');
+			$table->string('title');
+			$table->json('gana');
+			$table->json('kanji');
+			$table->json('meaning');
+			$table->boolean('is_public')->default(false);
+			$table->boolean('is_creator')->default(false);
+			$table->timestamps();
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('vocabulary_notes');
-    }
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void
+	{
+		Schema::dropIfExists('vocabulary_notes');
+	}
 };
