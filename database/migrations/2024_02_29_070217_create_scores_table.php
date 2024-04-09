b9e73250be98158c50163868277fba51e4f2f718
YooDaehan <daehan821@g.yju.ac.kr>
2024-04-09 10:45:16 +0900

    🔨 Refactor : Enums 분리 작업 및 분리에 따른 모델 및 마이그레이션 수정

--- database/migrations/2024_02_29_070217_create_scores_table.php
+++ database/migrations/2024_02_29_070217_create_scores_table.php
@@ -7,26 +7,26 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-  /**
-   * Run the migrations.
-   */
-  public function up(): void
-  {
-    Schema::create('scores', function (Blueprint $table) {
-      $table->id();
-      $table->foreignId('user_id')->constrained('users')->onDelete('cascade')->onUpdate('cascade');
-      $table->foreignId('level_id')->constrained('levels')->onDelete('cascade')->onUpdate('cascade');
-      $table->integer('score');
-      $table->string('category')->default(CategoryEnum::JLPT->value);
-      $table->timestamps();
-    });
-  }
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void
+	{
+		Schema::create('scores', function (Blueprint $table) {
+			$table->id();
+			$table->foreignId('user_id')->constrained('users')->onDelete('cascade')->onUpdate('cascade');
+			$table->foreignId('level_id')->constrained('levels')->onDelete('cascade')->onUpdate('cascade');
+			$table->integer('score');
+			$table->string('category')->default(CategoryEnum::JLPT->value);
+			$table->timestamps();
+		});
+	}
 
-  /**
-   * Reverse the migrations.
-   */
-  public function down(): void
-  {
-    Schema::dropIfExists('rankings');
-  }
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void
+	{
+		Schema::dropIfExists('scores');
+	}
 };
