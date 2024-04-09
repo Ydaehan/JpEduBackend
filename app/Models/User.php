139734e7219c3a7ad43eb415420bed63d6cdf1c7
SungHwan Sim <121005570+simsunghwan@users.noreply.github.com>
2024-04-09 10:50:49 +0900

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -73,7 +73,7 @@ class User extends Authenticatable
     return $this->hasOne(UserSetting::class);
   }
 
-  public function rankings(): HasMany
+  public function scores(): HasMany
   {
     return $this->hasMany(Score::class);
   }
