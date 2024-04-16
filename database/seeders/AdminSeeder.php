e50fa68c308bd05813bbda9969e7c6debfd1f407
simsunghwan <sunghwan1332@g.yju.ac.kr>
2024-04-16 11:50:35 +0900

    🔨 Refactor : 데이터베이스 시더 수정

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -8,31 +8,27 @@ use Illuminate\Database\Seeder;
 
 class AdminSeeder extends Seeder
 {
-	/**
-	 * Run the database seeds.
-	 */
-	public function run(): void
-	{
-		User::create([
-			'nickname' => 'Daehan',
-			'email' => 'daehan821@naver.com',
-			'password' => bcrypt('qwezxc123'),
-			'phone' => '01068089833',
-			'birthday' => '2000-05-19',
-			'role' => 'admin'
-		])->userSetting()->create([
-			'user_id' => '1',
-		]);
+  /**
+   * Run the database seeds.
+   */
+  public function run(): void
+  {
+    User::create([
+      'nickname' => 'Daehan',
+      'email' => 'daehan821@naver.com',
+      'password' => bcrypt('qwezxc123'),
+      'phone' => '01068089833',
+      'birthday' => '2000-05-19',
+      'role' => 'admin'
+    ])->userSetting()->create([]);
 
-		User::create([
-			'nickname' => 'Sunghwan',
-			'email' => 'sunghwan1332@gmail.com',
-			'password' => bcrypt('qwezxc123'),
-			'phone' => '01067831332',
-			'birthday' => '2000-07-24',
-			'role' => 'admin'
-		])->userSetting()->create([
-			'user_id' => '2',
-		]);
-	}
+    User::create([
+      'nickname' => 'Sunghwan',
+      'email' => 'sunghwan1332@gmail.com',
+      'password' => bcrypt('qwezxc123'),
+      'phone' => '01067831332',
+      'birthday' => '2000-07-24',
+      'role' => 'admin'
+    ])->userSetting()->create([]);
+  }
 }
